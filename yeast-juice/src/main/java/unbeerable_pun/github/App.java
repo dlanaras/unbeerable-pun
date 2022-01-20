77502102d22cb1cbf6aa4130d95d622c027e3802
dlanaras <dimitrislanaras04@outlook.com>
2022-01-20 16:25:21 +0100

    Merge branch 'main' of https://github.com/dlanaras/unbeerable-pun

--- yeast-juice/src/main/java/unbeerable_pun/github/App.java
+++ yeast-juice/src/main/java/unbeerable_pun/github/App.java
@@ -1,17 +1,29 @@
 package unbeerable_pun.github;
 
 import java.io.IOException;
-
+import org.apache.log4j.Logger;  
+import org.apache.log4j.LogManager;
 /**
  * @author: chads
  *
  */
 public class App {
+
+    private static final Logger log = LogManager.getLogger(App.class);
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-        BeerAdmin.loadConfig();
-        BeerAdmin.getBeerListForStyle(5);
-        BeerAdmin.printBeerList();
-        BeerAdmin.printBeer("xwYSL2");
+        log.trace("Entering application.");
+        try {
+            BeerAdmin.loadConfig();
+            BeerAdmin.getBeerListForStyle(5);
+            BeerAdmin.printBeerList();
+            BeerAdmin.printBeer("xwYSL2");
+        } catch(IOException e) {
+            //log.info("Error happened while reading config file");
+            //log.debug("Error happened while reading config file, more details: " + e);
+        }
+
+        log.trace("Exiting application.");
+
     }
 }
